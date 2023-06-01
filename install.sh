2eb21cd39c50a12b27c64bf50059bf77fe97c0dd
NicolasSoulay <soulaynicolas@gmail.com>
2023-06-01 14:31:37 +0200

    test

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd ~
 
 sudo apt update
 sudo apt upgrade
-sudo apt install picom lightdm git nitrogen thunderbird exa neofetch rofi numlockx libreoffice pulseaudio pavucontrol clang xsel python3 python3-pip ripgrep gcc g++ composer gh fd-find
+sudo apt install kitty wget picom lightdm git nitrogen thunderbird exa neofetch rofi steam numlockx libreoffice pulseaudio pavucontrol clang xsel python3 python3-pip python3-venv ripgrep gcc g++ composer gh fd-find
 sudo apt-get install ninja-build gettext cmake unzip curl
 
 gh auth login
@@ -23,20 +23,11 @@ rm -R neovim
 
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r pynvim
+pip install pynvim
 
 mkdir .config
-touch .config/user-dirs.dirs
-echo 'XDG_DESKTOP_DIR="$HOME/Desktop"' >> .config/user-dirs.dirs
-echo 'XDG_DOWNLOAD_DIR="$HOME/Downloads"' >> .config/user-dirs.dirs
-echo 'XDG_TEMPLATES_DIR="$HOME/Templates"' >> .config/user-dirs.dirs
-echo 'XDG_PUBLICSHARE_DIR="$HOME/Public"' >> .config/user-dirs.dirs
-echo 'XDG_DOCUMENT_DIR="$HOME/Documents"' >> .config/user-dirs.dirs
-echo 'XDG_MUSIC_DIR="$HOME/Music"' >> .config/user-dirs.dirs
-echo 'XDG_PICTURES_DIR="$HOME/Pictures"' >> .config/user-dirs.dirs
-echo 'XDG_VIDEOS_DIR="$HOME/Videos"' >> .config/user-dirs.dirs
 
-xdg-users-dirs-update
+xdg-user-dirs-update
 
 mkdir Dev
 
@@ -53,3 +44,10 @@ cd ~/.local/share
 git clone https://github.com/NicolasSoulay/fonts.git
 cd ~
 fc-cache -f -v
+
+wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
+sudo apt install ./discord.deb
+
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
